391f148e5ccc2763a5f73e1221debc98004cacb0
Mithlesh341 <mithleshmourya2004@gmail.com>
2025-09-03 12:31:41 +0530

    cleaned

--- frontend/task/src/App.jsx
+++ frontend/task/src/App.jsx
@@ -1,32 +1,3 @@
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Navigate,
-// } from "react-router-dom";
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
-// import Dashboard from "./pages/Dashboard";
-// import './App.css'
-
-// function App() {
-//   const token = localStorage.getItem("token");
-
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/signup" element={<Signup />} />
-//         <Route
-//           path="/"
-//           element={token ? <Dashboard /> : <Navigate to="/login" />}
-//         />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Login from "./pages/Login";
@@ -37,7 +8,6 @@ import './App.css'
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token"));
 
-  // Optional: update token from localStorage if changed in another tab
   useEffect(() => {
     const handleStorageChange = () => setToken(localStorage.getItem("token"));
     window.addEventListener("storage", handleStorageChange);
